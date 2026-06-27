926b1361adaad8396eecd33fa6915024079a113e
Paula Rojas <pa.rojaso@duocuc.cl>
2026-06-27 10:38:23 -0400

    Primer commit evaluacion 3

---
+++ app.py
@@ -0,0 +1,14 @@
+print("===================================")
+print("        HOLA MUNDO PYTHON")
+print("===================================")
+
+print(f"Usuario       : {usuario}")
+print(f"Hostname      : {hostname}")
+print(f"Sistema       : {sistema}")
+print(f"Versión       : {version}")
+print(f"Arquitectura  : {arquitectura}")
+print(f"Procesador    : {procesador}")
+
+print("===================================")
+print("Programa ejecutado correctamente")
+print("===================================")
